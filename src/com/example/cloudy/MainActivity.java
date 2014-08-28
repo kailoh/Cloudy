24392c2a617eed07256a40c09b3fba3b50f55eb7
Kai Herng Loh <kloh@Kais-MacBook-Pro.local>
2014-08-27 23:51:35 -0700

    add login using google account

--- src/com/example/cloudy/MainActivity.java
+++ src/com/example/cloudy/MainActivity.java
@@ -1,18 +1,65 @@
 package com.example.cloudy;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi.DriveIdResult;
+import com.google.android.gms.drive.DriveApi.MetadataBufferResult;
+import com.google.android.gms.drive.DriveFolder;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends BaseActivity {
+	
+	private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate");
         setContentView(R.layout.activity_main);
     }
+    
+    @Override
+    public void onConnected(Bundle connectionHint) {
+    	super.onConnected(connectionHint);
+    	login();
+    	
+    }
+    
+    public void login() {
+        Drive.DriveApi.fetchDriveId(getGoogleApiClient(), EXISTING_FOLDER_ID)
+                .setResultCallback(idCallback);
+    }
+
+    final private ResultCallback<DriveIdResult> idCallback = new ResultCallback<DriveIdResult>() {
+        @Override
+        public void onResult(DriveIdResult result) {
+            if (!result.getStatus().isSuccess()) {
+                showMessage("Cannot find DriveId. Are you authorized to view this file?");
+                return;
+            }
+            DriveFolder folder = Drive.DriveApi.getFolder(getGoogleApiClient(), result.getDriveId());
+            folder.listChildren(getGoogleApiClient())
+                    .setResultCallback(metadataResult);
+        }
+    };
+
+    final private ResultCallback<MetadataBufferResult> metadataResult = new
+            ResultCallback<MetadataBufferResult>() {
+        @Override
+        public void onResult(MetadataBufferResult result) {
+            if (!result.getStatus().isSuccess()) {
+                showMessage("Problem while retrieving files");
+                return;
+            }
+            Log.e(TAG, "Result: " + result.getMetadataBuffer());
+            showMessage("Successfully listed files.");
+        }
+    };
 
 
     @Override
